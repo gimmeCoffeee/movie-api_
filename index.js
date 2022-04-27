4bd15d0ae835ca97f528b5a8e7a0daa3e7f928c3
gimmeCoffeee <93482616+gimmeCoffeee@users.noreply.github.com>
2022-04-26 23:11:17 -0400

    updated json packages and added some code to index.js

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my book club!');
+});
+
+app.get('/documentation', (req, res) => {
+    res.sendFile('public/documentation.html', {root: __dirname});
+});
+
+app.get('/books', (req, res) => {
+    res.json(topMovies);
+});
+
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
+
+app.use(express.static('public'));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something Broke!');
+});
