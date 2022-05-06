f52c9fe21b7984b52e51147f9289378a7942f4c0
gimmeCoffeee <93482616+gimmeCoffeee@users.noreply.github.com>
2022-05-05 20:59:46 -0400

    added a call for morgan and mog=rgan middleware for logging

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express');
+      morgan = require('morgan');
+
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Welcome to my favorite movie list!');
+    res.send('Welcome to my top 10 movie list!');
 });
+
+app.use(express.static('public'));
 
 app.get('/documentation', (req, res) => {
     res.sendFile('public/documentation.html', {root: __dirname});
 });
 
 app.get('/movies', (req, res) => {
-    res.sendFile('topmovies.js', {root: __dirname});
-});
+    res.json(movies);
+  });
 
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
